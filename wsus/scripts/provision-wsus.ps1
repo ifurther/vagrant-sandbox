9c366da8721d280ed0587990fe0a9590f4aba7e9
ifurther <55025025+ifurther@users.noreply.github.com>
2022-07-05 13:49:36 +0800

    update firewall command. ref:'https://go.microsoft.com/fwlink/\?linkid\=121488'

--- wsus/scripts/provision-wsus.ps1
+++ wsus/scripts/provision-wsus.ps1
@@ -1,5 +1,7 @@
 # open ping for packer-windows builds, this deprecated command even works on 2012 R2
-netsh firewall set icmpsetting 8
+#netsh firewall set icmpsetting 8
+# new command
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
 
 . C:\vagrant\scripts\install-wsus.ps1
 . C:\vagrant\scripts\configure-wsus.ps1
